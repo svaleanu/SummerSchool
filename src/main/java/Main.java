1f43789d9cf71a30e3d65f97d5a827117f2ab4bf
svaleanu <svaleanu@deloittece.com>
2021-08-03 09:56:53 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e = new Employee("John",25);
+        Gson g = new Gson();
+        String json = g.toJson(e);
+
+        System.out.println(json);
+    }
+
+    public static class Employee {
+        private String name;
+        private int age;
+
+        public Employee(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+}
